2072cc0fd7b65a6fb38fc55172734fe560c17175
Castercodex <edwardberth1@gmail.com>
2020-10-09 02:01:58 +0100

    third commit

--- Xplorer/settings.py
+++ Xplorer/settings.py
@@ -140,9 +140,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )

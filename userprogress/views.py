09ae0dc2d4edfa26dd6f260d4864ead2e3346abc
Castercodex <edwardberth1@gmail.com>
2020-10-23 11:39:11 +0100

    almost-done

--- userprogress/views.py
+++ userprogress/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from .models import Course, EnrolledCourse, CompletedCourse
 
+
 # Create your views here.
 
 
@@ -17,21 +18,21 @@ def add_to_enrolled(request, slug):
         course=course,
         user=request.user,
     )
-    enrolled_qs = EnrolledCourse.objects.filter()
-    if enrolled_qs.exists():
-        enrolled = enrolled_qs[0]
-        # check if the order item is in the order
-        if enrolled.course.filter(course__slug=course.slug).exists():
-            messages.info(request, "This item quantity Exist")
-            # return redirect("core:order-summary")
-        else:
-            enrolled.course.add(enrolled_course)
-            messages.info(request, "This item was added to your cart.")
-    else:
-        enrolled_date = timezone.now()
-        enrolled = EnrolledCourse.objects.create(
-            user=request.user, enrolled_date=enrolled_date
-        )
-        enrolled.course.add(enrolled_course)
-        messages.info(request, "This item was added to your cart.")
-        return redirect("/dashboard")
+    # enrolled_qs = EnrolledCourse.objects.filter()
+    # # if enrolled_qs.exists():
+    # #     enrolled = enrolled_qs
+    # #     # check if the order item is in the order
+    # #     if enrolled.filter(course__slug=course.slug).exists():
+    # #         messages.info(request, "This item quantity Exist")
+    # #         # return redirect("core:order-summary")
+    # #     else:
+    # #         enrolled.course.add(enrolled_course)
+    # #         messages.info(request, "This item was added to your cart.")
+    # # else:
+    # #     enrolled_date = timezone.now()
+    # #     enrolled = EnrolledCourse.objects.create(
+    # #         user=request.user, enrolled_date=enrolled_date
+    # #     )
+    # #     enrolled.course.add(enrolled_course)
+    # #     messages.info(request, "This item was added to your cart.")
+    return HttpResponseRedirect(redirect_to="/dashboard")
